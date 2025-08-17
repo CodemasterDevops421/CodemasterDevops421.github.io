57491d45982027c00404753f82d6eee8f9c56b22
Codemaster_421 <melamchaitanyakumar@gmail.com>
2025-08-17 19:00:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 const repo = process.env.GITHUB_REPOSITORY?.split('/')?.[1]
-const isUserSite = repo && repo.endsWith('.github.io')
+const isUserSite = repo && repo.toLowerCase().endsWith('.github.io')
 
 export default defineConfig({
   plugins: [react()],
